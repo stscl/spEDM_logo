97e169d2fc7bfac4728eef93c24de4d29b98b83f
SpatLyu <lyu.geosocial@gmail.com>
2024-10-06 12:03:19 +0800

    first commit

---
+++ spEDM_logo.R
@@ -0,0 +1,58 @@
+# setwd('../../Downloads/materials/spEDM_logo/')
+
+library(ggplot2)
+set.seed(123)
+fig1 = ggplot(data.frame(x = rnorm(5e5),y = rnorm(5e5)), 
+              aes(x = x, y = y)) +
+  geom_hex(bins = 30, show.legend = F) +
+  scale_fill_gradient(low = "#212d2c", high = "#ffffff") + 
+  coord_fixed() + 
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "transparent", color = "transparent"),
+    panel.background = element_rect(fill = "transparent", color = "transparent")
+  ) +
+  ggview::canvas(width = 600, height = 550, units = 'px')
+ggview::save_ggplot(fig1,'./bg1.png',dpi = 100, bg = 'transparent')
+
+library(ggimage)
+library(showtext)
+showtext_auto(enable = TRUE)
+font_add("ShineTypewriter", regular = "./ShineTypewriter-lgwzd.ttf")
+library(hexSticker)
+library(magick)
+
+ccm = figpatch::fig('./CCM1.png')
+ccm = ggbackground(ccm, './bg1.png')
+  
+ggplot2::ggsave('./ccm_brick1.png',ccm,
+                width = 3.25, height = 3.25, 
+                dpi = 100)
+
+sticker(
+  subplot = "./ccm_brick1.png",
+  s_x = 0.995,
+  s_y = 0.920,
+  s_width = .6,
+  s_height = .6,
+  package = "spEDM",
+  p_family = "ShineTypewriter",
+  p_size = 17.5,
+  p_x = 1.00,
+  p_y = 1.60,
+  p_color = ggplot2::alpha("#a9fdff",1),
+  dpi = 300,
+  asp = 1,
+  h_size = 2.55,
+  h_color = ggplot2::alpha("#24e2be",75),
+  h_fill = '#212d2c',
+  white_around_sticker = F,
+  url = "https://stscl.github.io/spEDM",
+  u_color = ggplot2::alpha("#a9fdff",.75),
+  u_size = 5.25,
+  filename = "spEDM_logo.png"
+)
+
+image_read('./spEDM_logo.png') |> 
+  image_resize("256x256")|> 
+  image_write('./spEDM_logo.png')
