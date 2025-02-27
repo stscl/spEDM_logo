dccbb71d184e8e811b8f7119542d254baff1e77f
SpatLyu <lyu.geosocial@gmail.com>
2025-02-27 21:51:17 +0800

    update figures

---
+++ phase_space.r
@@ -0,0 +1,8 @@
+cu = terra::rast(system.file("extdata/cu.tif", package = "spEDM"))
+
+m1 = spEDM::embedded(cu,"industry",E = 9,tau = 0,trend.rm = FALSE)
+m2 = spEDM::embedded(cu,"cu",E = 9,tau = 0,trend.rm = FALSE)
+colnames(m1) = colnames(m2) = paste0("z",1:9)
+
+readr::write_csv(as.data.frame(m1),'./result/phase_space1.csv')
+readr::write_csv(as.data.frame(m2),'./result/phase_space2.csv')
